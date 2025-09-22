12e0165ecfab8dd9e752a7ed7f4c73a12c6f1275
jesse <u1110728@utah.edu>
2025-09-21 20:17:23 -0600

    --Added multer for image uploading. --BusinessRoute.ts now taking multipart form data --BusinessRoute.ts now getting data from admin app.

--- src/Routes/BusinessRoute.ts
+++ src/Routes/BusinessRoute.ts
@@ -1,12 +1,17 @@
 import { verify } from 'crypto';
 import express from 'express';
+import multer from 'multer';
 
 import verifyGoogleToken from '../../Controllers/GoogleTokenAuthentication';
 
 const router = express.Router();
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage})
 
-router.post('/addbusiness', async(req, res) => {
+router.post('/addbusiness', upload.array("image", 4), async(req, res) => {
     console.log("hitting server");
+    console.log(req.body);
+    console.log(req.files);
 });
 
 export default router;
