5adafdea18479f9a2750aba10b5fab0980483de2
jesse <u1110728@utah.edu>
2025-10-25 17:04:35 -0600

    --Business.ts now checks for req files size for safe checking. --A more meaningful message, id , and business name response is now being sent to the client.

--- Controllers/Business.ts
+++ Controllers/Business.ts
@@ -16,99 +16,64 @@ interface MulterRequest extends Request {
 
 
 export const addBusiness = async (req: MulterRequest, res: any) => {
-   
+
     try {
 
         const emailExists = await Business.exists({ email: req.body.email });
         if (emailExists) {
-            return res.status(400).json({ error: "Business and/or Email already exists" });
-        } else {
-            const business = new Business({
-                name: req.body.name,
-                email: req.body.email,
-                phone: req.body.phone,
-                street: req.body.street,
-                city: req.body.city,
-                state: req.body.state,
-                zipcode: req.body.zipcode,
+            return res.status(400).json({ 
+                success: false,
+                message: "Business and/or Email already exists" });
+        }
 
+        const business = new Business({
+            name: req.body.name,
+            email: req.body.email,
+            phone: req.body.phone,
+            street: req.body.street,
+            city: req.body.city,
+            state: req.body.state,
+            zipcode: req.body.zipcode,
+        });
+
+        const saved = await business.save();
+
+        if (req.files && Array.isArray(req.files) && req.files.length > 0) {
+
+            const bucketName = "small-market-bucket1";
+            const client = new S3Client({
+                region: 'us-west-2',
+                credentials: {
+                    accessKeyId: process.env.AWS_S3_ACCESS_KEY!,
+                    secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY!,
+                }
             });
 
-            const saved = await business.save();
+            const uploadPromises = req.files?.map(async (file: Express.Multer.File) => {
 
-            console.log(saved);
+                const key = `business/images/${saved.id}_${file.originalname}`;
 
-            if (req.files) {
-
-                const bucketName = "small-market-bucket1";
-                const client = new S3Client({
-                    region: 'us-west-2',
-                    credentials: {
-                        accessKeyId: process.env.AWS_S3_ACCESS_KEY!,
-                        secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY!,
-                    }
+                const command = new PutObjectCommand({
+                    Bucket: bucketName,
+                    Key: key,
+                    Body: file.buffer,
+                    ContentType: file.mimetype,
                 });
 
-                const uploadPromises = req.files?.map(file => {
-                    console.log(file.originalname)
-                    
-                    const key = `${bucketName}/business/images/${saved.id}_${file.originalname}`;
+                return client.send(command);
+            });
 
-                    const command = new PutObjectCommand({
-                        Bucket: bucketName,
-                        Key: key,
-                        Body: file.buffer,
-                        ContentType: file.mimetype,
-                    });
-
-                    return client.send(command);
-                });
-
-                const uploadedFiles = await Promise.all(uploadPromises);
-                res.status(200).json({
-                    message: "Files uploaded successfully",
-                });
-            }
+            const uploadedFiles = await Promise.all(uploadPromises);
         }
+        
+         res.status(200).json({
+                message: "Business created successfully",
+                id: saved._id.toString(),
+                name: saved.name
+            });
 
     } catch (error) {
         console.log(error);
     }
-    // if (req.files) {
-    //     try{
-    //     const bucketName = "small-market-bucket1";
-    //     const client = new S3Client({
-    //         region: 'us-west-2',
-    //         credentials: {
-    //             accessKeyId: process.env.AWS_S3_ACCESS_KEY!,
-    //             secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY!,
-    //         }
-    //     });
-
-    //     const uploadPromises = req.files?.map(file => {
-    //         console.log(file.originalname)
-    //         console.log(req);
-    //         const key = `${bucketName}/business/images/${file.originalname}`;
-
-    //         const command = new PutObjectCommand({
-    //             Bucket: bucketName,
-    //             Key: key,
-    //             Body: file.buffer,
-    //             ContentType: file.mimetype,
-    //         });
-
-    //         return client.send(command);
-    //     });
-
-    // const uploadedFiles = await Promise.all(uploadPromises);
-    // res.status(200).json({
-    //     message: "Files uploaded successfully",
-    //    });
-    //     console.log("hitting server");
-    // } catch(error){
-    //     console.log(error);
-    // }
-    //}
-
 }
 
