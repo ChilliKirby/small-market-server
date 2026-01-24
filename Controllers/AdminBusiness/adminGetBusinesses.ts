6505b443b9127d8409efefb10275d565300d2a56
jesse <u1110728@utah.edu>
2026-01-23 18:28:10 -0700

    --

--- Controllers/AdminBusiness/adminGetBusinesses.ts
+++ Controllers/AdminBusiness/adminGetBusinesses.ts
@@ -1,22 +1,35 @@
 import Business from '../../Models/Business';
 import { Router, Request, Response } from 'express';
 
-const adminGetBusinesses = async(req: Request, res: Response): Promise<void> => {
-    
+const adminGetBusinesses = async (req: Request, res: Response): Promise<void> => {
+
     const limit = 15;
     const page = Number(req.query.page || 1);
-    const skip = (page-1) * limit;
-    try{
-        const response = await Business
-        .find({ status: 'approved' })
-        .sort({ name: 1, _id: 1})
-        .skip(skip)
-        .limit(limit)
-        .lean();
-
-        console.log(response);
-    } catch(error){
+    const skip = (page - 1) * limit;
+    try {
+        const [response, total] = await Promise.all([
+            Business
+                .find({ status: 'approved' })
+                .sort({ name: 1, _id: 1 })
+                .skip(skip)
+                .limit(limit)
+                .lean(),
+            Business.countDocuments(),
+        ])
+        
+        res.json({
+            response,
+            page,
+            total,
+            hasMore: skip + adminGetBusinesses.length < total,
+        });
+    } catch (error) {
         console.log(error);
+        res.status(200).json({
+            businesses: [],
+            page,
+            total: 0,
+        })
     }
 };
 
