31bd8825b1818b45990e24ce3927f14549bb62da
jesse <u1110728@utah.edu>
2026-04-05 17:27:59 -0600

    --added adminUpdateBusinessImage to route

--- src/Routes/AdminBusinessRoute.ts
+++ src/Routes/AdminBusinessRoute.ts
@@ -8,6 +8,7 @@ import {addBusiness} from '../../Controllers/AdminBusiness/adminAddBusiness';
 import adminGetBusinesses from '../../Controllers/AdminBusiness/adminGetBusinesses';
 import adminGetBusiness from '../../Controllers/AdminBusiness/adminGetBusiness';
 import adminUpdateBusiness from '../../Controllers/AdminBusiness/adminUpdateBusiness';
+import adminUpdateBusinessImage from '../../Controllers/AdminBusiness/adminUpdateBusinessImage';
 
 const router = express.Router();
 const storage = multer.memoryStorage();
@@ -18,4 +19,6 @@ router.post('/addbusiness', upload.single("image"), addBusiness);
 router.put('/adminupdatebusiness/:id', jwtAuthentication, adminUpdateBusiness);
 router.get('/admingetbusinesses',jwtAuthentication, adminGetBusinesses);
 router.get('/admingetbusiness', jwtAuthentication, adminGetBusiness);
+router.get('/adminupdatebusinessimage/:id', upload.single("image"), adminUpdateBusinessImage);
+
 export default router;
