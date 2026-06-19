35c2977be7d4c6b795181146f41af907bf4f6d97
jesse <u1110728@utah.edu>
2026-06-18 21:47:50 -0600

    --UsersGetBusinessesWithCategories written. Needs testing

--- Controllers/UsersBusiness/UsersGetBusinessesWithCategories.ts
+++ Controllers/UsersBusiness/UsersGetBusinessesWithCategories.ts
@@ -9,6 +9,7 @@ const usersGetBusinessesWithCategories = async (req: Request, res: Response) => 
     const page = Number(req.query.page || 1);
     const skip = (page - 1) * limit;
     const baseUrl = process.env.AWS_IMAGE_URL;
+    console.log("in user cat bus")
     try{
         const [businesses, businessCount] = await Promise.all([
             Business.find({ categoryIds: category })
@@ -27,6 +28,11 @@ const usersGetBusinessesWithCategories = async (req: Request, res: Response) => 
             imageMain: business.imageMain != null ?
             baseUrl + business.imageMain : null,
         }));
+
+        return res.status(200).json({
+            businesses: businesses,
+            businessCount: businessCount
+        })
 
     } catch(error){
         return res.status(200);
